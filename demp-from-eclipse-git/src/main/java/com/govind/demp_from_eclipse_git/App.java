e20c2acb36d333ecbbaabeb6ed346f3e204f82eb
GovindK1143 <govind.satyam@gmail.com>
2023-12-08 01:16:51 +0530

    first commit

---
+++ demp-from-eclipse-git/src/main/java/com/govind/demp_from_eclipse_git/App.java
@@ -0,0 +1,13 @@
+package com.govind.demp_from_eclipse_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
